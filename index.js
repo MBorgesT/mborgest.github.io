ddd905bd76290aad7006e0dd5d5ead9f044770e2
Matheus Teixeira <matheus@agrippacapital.io>
2026-03-03 10:02:25 +0000

    testing loading message

--- index.js
+++ index.js
@@ -46,6 +46,7 @@ for (let i = 0; i < 13; i++) {
 
 mosaicContainer = document.getElementById('infrared-mosaic-container');
 await createMosaicElement(photoList, mosaicContainer);
+document.getElementById('loading-infrared').style.display = 'none';
 
 // People section
 photoList = Array(14);
@@ -55,6 +56,7 @@ for (let i = 0; i < 14; i++) {
 
 mosaicContainer = document.getElementById('people-mosaic-container');
 await createMosaicElement(photoList, mosaicContainer);
+document.getElementById('loading-people').style.display = 'none';
 
 // Portraits section
 photoList = Array(9);
@@ -64,6 +66,7 @@ for (let i = 0; i < 9; i++) {
 
 mosaicContainer = document.getElementById('portraits-mosaic-container');
 await createMosaicElement(photoList, mosaicContainer);
+document.getElementById('loading-portraits').style.display = 'none';
 
 // Landscapes/Other section
 photoList = Array(15);
@@ -73,3 +76,4 @@ for (let i = 0; i < 15; i++) {
 
 mosaicContainer = document.getElementById('landscapes-other-mosaic-container');
 await createMosaicElement(photoList, mosaicContainer);
+document.getElementById('loading-landscapes-other').style.display = 'none';
